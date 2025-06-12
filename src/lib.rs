4cd6ad28cf29c920a1dbd1cdb0ad892394fec620
AryaveerSR <51504825+AryaveerSR@users.noreply.github.com>
2025-06-12 10:10:28 +0530

    serve public directory

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ mod api;
 mod domain;
 mod telemetry;
 
-use axum::{Router, middleware, routing::get};
+use axum::{Router, middleware};
 use sqlx::{Pool, Sqlite};
+use tower_http::services::ServeDir;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -14,16 +15,15 @@ pub async fn init(pool: Pool<Sqlite>) -> anyhow::Result<Router> {
     // Init logging
     let _ = tracing_subscriber::fmt::try_init();
 
+    // Serving static files
+    let serve_dir = ServeDir::new("public");
+
     // Build router
     let router = Router::new()
         .nest("/api", api::routes())
-        .route("/", get(root))
+        .fallback_service(serve_dir)
         .with_state(AppState { pool })
         .layer(middleware::from_fn(telemetry::middleware));
 
     Ok(router)
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
 }
