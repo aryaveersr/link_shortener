d4f6eeb430ce5ae7d16e00a09e033d4915953f22
AryaveerSR <51504825+AryaveerSR@users.noreply.github.com>
2025-06-14 20:27:49 +0530

    refactor 404 and /to

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,18 +1,18 @@
-mod _404;
 mod api;
 mod to;
 
 use crate::AppState;
-use axum::{Router, handler::HandlerWithoutStateExt, routing::get};
-use tower_http::services::ServeDir;
+use axum::{Router, routing::get};
+use tower_http::services::{ServeDir, ServeFile};
 
 pub fn routes() -> Router<AppState> {
     // Serving static files
-    let serve_dir = ServeDir::new("public");
+    let serve_public_dir = ServeDir::new("public");
+    let serve_404 = ServeFile::new("public/404.html");
 
     // Create router
     Router::new()
         .route("/to/{slug}", get(to::handler))
         .nest("/api", api::routes())
-        .fallback_service(serve_dir.fallback(_404::handler.into_service()))
+        .fallback_service(serve_public_dir.not_found_service(serve_404))
 }
--- src/routes/to.rs
+++ src/routes/to.rs
@@ -1,81 +1,57 @@
-use crate::{
-    AppState,
-    domain::{Href, LinkEntry, Slug},
-    routes::_404::HtmlTemplate,
-};
-use anyhow::{Context, anyhow};
-use askama::Template;
+use crate::{AppState, domain::Slug};
 use axum::{
-    extract::{Path, Request, State},
+    extract::{Path, State},
     http::StatusCode,
     response::{Html, IntoResponse, Redirect, Response},
 };
 use sqlx::{Pool, Sqlite};
-use tracing::{debug, error};
+use tracing::error;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ResponseError {
     #[error("Requested slug does not exist")]
-    NotFound(String),
+    NotFound,
 
     #[error(transparent)]
-    UnexpectedError(#[from] anyhow::Error),
+    UnexpectedError(#[from] sqlx::Error),
 }
 
 impl IntoResponse for ResponseError {
     fn into_response(self) -> Response {
         match self {
             Self::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-
-            Self::NotFound(path) => {
-                debug!(path, "Rendering 404.html");
-
-                let template = HtmlTemplate { path };
-
-                match template.render() {
-                    Ok(html) => (StatusCode::NOT_FOUND, Html(html)).into_response(),
-                    Err(err) => {
-                        error!(?err, "Template rendering failed");
-
-                        StatusCode::INTERNAL_SERVER_ERROR.into_response()
-                    }
-                }
-            }
+            Self::NotFound => (
+                StatusCode::NOT_FOUND,
+                Html(include_str!("../../public/404.html")),
+            )
+                .into_response(),
         }
-    }
-}
-
-#[tracing::instrument(skip(pool, request))]
-pub async fn handler(
-    State(AppState { pool }): State<AppState>,
-    Path(slug): Path<String>,
-    request: Request,
-) -> Result<Redirect, ResponseError> {
-    let path = request.uri().path();
-
-    let parsed_slug = Slug::parse(slug).map_err(|_| ResponseError::NotFound(path.into()))?;
-
-    match fetch_slug(&pool, parsed_slug).await? {
-        Some(link_entry) => Ok(Redirect::temporary(link_entry.href.as_ref())),
-        None => Err(ResponseError::NotFound(path.into())),
     }
 }
 
 #[tracing::instrument(skip(pool))]
-async fn fetch_slug(pool: &Pool<Sqlite>, slug: Slug) -> anyhow::Result<Option<LinkEntry>> {
+pub async fn handler(
+    State(AppState { pool }): State<AppState>,
+    Path(slug): Path<String>,
+) -> Result<Redirect, ResponseError> {
+    let slug = Slug::parse(slug).or(Err(ResponseError::NotFound))?;
+
+    match fetch_href_for_slug(&pool, &slug).await? {
+        Some(href) => Ok(Redirect::temporary(&href)),
+        None => Err(ResponseError::NotFound),
+    }
+}
+
+#[tracing::instrument(skip(pool))]
+async fn fetch_href_for_slug(
+    pool: &Pool<Sqlite>,
+    slug: &Slug,
+) -> Result<Option<String>, sqlx::Error> {
     let slug_ref = slug.as_ref();
 
     let record = sqlx::query!("SELECT * FROM links WHERE slug = $1;", slug_ref)
         .fetch_optional(pool)
-        .await
-        .context("Failed to execute query")?;
+        .await?;
 
-    match record {
-        Some(rec) => {
-            let href = Href::parse(&rec.href).map_err(|err| anyhow!(err))?;
-
-            Ok(Some(LinkEntry { href, slug }))
-        }
-        None => Ok(None),
-    }
+    Ok(record.map(|r| r.href))
 }
